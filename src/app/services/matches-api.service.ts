6720cfd384f48f66973ec5d0ad635d2b9cb2324c
Fichtner21 <ernest.fichtner@gmail.com>
2021-12-12 16:13:24 +0100

    new method in matches api service

--- src/app/services/matches-api.service.ts
+++ src/app/services/matches-api.service.ts
@@ -12,8 +12,43 @@ export class MatchesApiService {
   public getMatches(name:string): Observable<any>{
     return this.http.get<any>(`https://sheets.googleapis.com/v4/spreadsheets/1w_WHqCutkp_S6KveKyu4mNaG76C5dIlDwKw-A-dEOLo/values/${name}?key=AIzaSyD6eJ4T-ztIfyFn-h2oDAGTnNNYhNRziLU`);
   }
+  
+  public addTitleCup(place:string) {
+    let cupInfoTitle = '';
+    switch (place) {
+      case '1': {
+        cupInfoTitle = `Winner in 1on1 CUP 1st Edition`;
+        break;
+      }
+      case '2': {
+        cupInfoTitle = `2nd place in 1on1 CUP 1st Edition`;
+        break;
+      }
+      case '3': {
+        cupInfoTitle = `3rd place in 1on1 CUP 1st Edition`;
+        break;
+      }
+      default:
+    }
+    return cupInfoTitle;
+  }
 
-  public getNumOfPlayers(arr:any){
-    return arr.length;
+  public findPlayerLastWar(name:string, obj:object) {    
+    const findeLastWar = Object.values(obj)
+      .filter((item) => JSON.stringify(item).includes(name))
+      .pop();    
+    let findLastTimeStamp = '';
+    let newTimestampElem = '';
+    if (findeLastWar) {
+      findLastTimeStamp = findeLastWar.timestamp;
+      newTimestampElem = new Date(findLastTimeStamp).toLocaleDateString('pl-PL', { hour: '2-digit', minute: '2-digit' });
+    } else if (findeLastWar === 'undefined') {
+      findLastTimeStamp = 'No match';
+    } else if (findeLastWar === null) {
+      findLastTimeStamp = 'No match';
+    } else {
+      findLastTimeStamp = 'No match';
+    }
+    return newTimestampElem;
   }
 }

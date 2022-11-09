6cdc58f0f44aca36ee8e2b56dfc8eee4ded7e914
Fichtner <etfe@gft.com>
2022-11-09 16:18:59 +0100

    fix single match map view

--- src/app/history-obj/single-match/single-match.component.ts
+++ src/app/history-obj/single-match/single-match.component.ts
@@ -18,10 +18,10 @@ import { CommentsService } from './comments.service';
 })
 export class SingleMatchComponent implements OnInit {
   public match$:Observable<Matches>;
-  public errorMessage: string; 
+  public errorMessage: string;
 
   public playersTab$: Observable<any>;
-  public matchesTab$: Observable<any>;  
+  public matchesTab$: Observable<any>;
   public inactiveTab$: Observable<any>;
   public historyObj$: any;
 
@@ -39,14 +39,14 @@ export class SingleMatchComponent implements OnInit {
   submitted = false;
 
   constructor(
-    private activatedRoute: ActivatedRoute, private tabApiService: PlayersApiService, private fetchMatch: FetchMatchesService, private afs: AngularFirestore, private commentService: CommentsService,  ) {  
+    private activatedRoute: ActivatedRoute, private tabApiService: PlayersApiService, private fetchMatch: FetchMatchesService, private afs: AngularFirestore, private commentService: CommentsService,  ) {
       this.singlePostCollection = afs.collection<SingleComment>('postComments');
-      // this.singleComment$ = this.singlePostCollection.valueChanges({idField: 'id'}) 
-      console.log('this.singlePostCollection', this.singlePostCollection);    
+      // this.singleComment$ = this.singlePostCollection.valueChanges({idField: 'id'})
+      console.log('this.singlePostCollection', this.singlePostCollection);
     }
-  
-  ngOnInit(): void {   
-   
+
+  ngOnInit(): void {
+
     console.log('getAll =>', this.commentService.getAll().valueChanges());
     this.allComments$ = this.commentService.getAll().valueChanges();
     // this.allComments$.pipe().subscribe((res) => console.log('res', res))
@@ -62,18 +62,18 @@ export class SingleMatchComponent implements OnInit {
     //     res = this.commentService.getAll();
     //     console.log('res', res)
     //   })
-    //  )) 
+    //  ))
     const matchID = this.activatedRoute.snapshot.params['idwar'];
     console.log('matchID', matchID);
     console.log('this.afs', this.afs.firestore);
     this.comment$ = this.afs.collection<SingleComment>('spearhead-mix-league').doc(matchID).valueChanges();
     console.log('this.comment', this.comment$);
     console.log('ressss', this.afs.collection<SingleComment>('spearhead-mix-league').doc(matchID))
-    
+
 
     this.match$ = this.activatedRoute.data.pipe(
-      map(data => data.match)       
-    );    
+      map(data => data.match)
+    );
 
     this.match$.pipe(
       map(x => this.matchVideo = x.video)).subscribe();
@@ -82,10 +82,10 @@ export class SingleMatchComponent implements OnInit {
       map((data) => {
         return data.match.idwar;
       })
-    );       
+    );
 
     this.playersTab$ = this.tabApiService.getPlayers('Players').pipe(
-      map((response: any) => {        
+      map((response: any) => {
         let batchRowValues = response.values;
         let players: any[] = [];
         for(let i = 1; i < batchRowValues.length; i++){
@@ -94,31 +94,31 @@ export class SingleMatchComponent implements OnInit {
             rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
           }
           players.push(rowObject);
-        }          
+        }
         return players;
       }),
     );
 
     this.matchesTab$ = this.tabApiService.getPlayers('Match+History').pipe(
-      map((response: any) => {   
-            
+      map((response: any) => {
+
         let batchRowValuesHistory = response.values;
-       
+
         let historyMatches: any[] = [];
         for(let i = 1; i < batchRowValuesHistory.length; i++){
           const rowObject: object = {};
           for(let j = 0; j < batchRowValuesHistory[i].length; j++){
             rowObject[batchRowValuesHistory[0][j]] = batchRowValuesHistory[i][j];
-          }          
-         
+          }
+
           historyMatches.push(rowObject);
-        }        
+        }
         return historyMatches;
       }),
-    );    
+    );
 
     this.inactiveTab$ = this.tabApiService.getPlayers('Inactive').pipe(
-      map((response: any) => {        
+      map((response: any) => {
         let batchRowValuesHistory = response.values;
         let inactivePlayers: any[] = [];
         for(let i = 1; i < batchRowValuesHistory.length; i++){
@@ -127,31 +127,31 @@ export class SingleMatchComponent implements OnInit {
             rowObject[batchRowValuesHistory[0][j]] = batchRowValuesHistory[i][j];
           }
           inactivePlayers.push(rowObject);
-        }        
+        }
         return inactivePlayers;
       }),
-    ); 
+    );
 
     this.historyObj$ = combineLatest([this.playersTab$, this.match$, this.inactiveTab$]).pipe(
       map(([players, match, inactive]) => {
         const singleMap = match.info;
         // console.log('match.info', match.info);
-       
+
         const csLewisQuote = match.info;
         const regex1 = /The Hunt/;
         const regex2 = /The Bridge, The Church Final/g;
         const regex3 = /The Church Final/g;
 
-        // console.log('regEx =>', csLewisQuote.match(match.info)); 
+        // console.log('regEx =>', csLewisQuote.match(match.info));
         const findMap = csLewisQuote.match(match.info);
 
         const mapArray = [];
         findMap.forEach((el) => {
           mapArray.push(el);
         });
-        
+
         const newMapArray =  mapArray[0].split(', ');
-        
+
         newMapArray.forEach((el, i) => {
         if(el == 'The Hunt'){
             const newEl = 'TheHunt';
@@ -198,13 +198,13 @@ export class SingleMatchComponent implements OnInit {
         // console.log('newMapArray', this.neMap);
 
         // this.gridTemplateColumns = { gridTemplateColumns: `${this.countColumns(this.neMap.length)}`};
-     
-        let matchRow;         
+
+        let matchRow;
           matchRow = {
             timestamp: match.timestamp,
             idwar: match.idwar,
             t1roundswon: match.t1roundswon,
-            t2roundswon: match.t2roundswon, 
+            t2roundswon: match.t2roundswon,
             video: 'https://www.youtube.com/embed/' + match.video + '/?autoplay=1',
             videoImg: 'https://img.youtube.com/vi/' + match.video + '/hqdefault.jpg',
             info: match.info,
@@ -279,15 +279,15 @@ export class SingleMatchComponent implements OnInit {
             t2p7preelo: match.t2p7preelo,
             t2p7score: match.t2p7score,
             t2p7postelo: match.t2p7postelo,
-          }          
+          }
           // console.log('matchRow', matchRow);
           console.log('thisSingle', this.commentService.getSingleComment(match.idwar).valueChanges())
           this.commentService.getSingleComment(match.idwar).valueChanges().pipe().subscribe(res => console.log('res', res));
-          
+
         return matchRow;
       })
-    );    
-    
+    );
+
   }
 
   addComment(id:string): void {
@@ -312,7 +312,7 @@ export class SingleMatchComponent implements OnInit {
   public neMap = [];
 
   public addPlayerLink(player:string, obj:any, obj2:any) {
-    let convertedPlayer = '';    
+    let convertedPlayer = '';
     obj.forEach((el:any) => {
       if (player === el.username) {
         convertedPlayer = el.playername;
@@ -332,24 +332,24 @@ export class SingleMatchComponent implements OnInit {
       }
     });
     return convertedPlayer;
-  } 
+  }
 
   countColumns(column:any){
     switch(column){
       case 1:
-        return 'repeat(1, 1fr)';       
+        return 'repeat(1, 1fr)';
       case 2:
-        return 'repeat(2, 1fr)';       
+        return 'repeat(2, 1fr)';
       case 3:
-        return 'repeat(3, 1fr)';       
+        return 'repeat(3, 1fr)';
       case 4:
-        return 'repeat(4, 1fr)';       
+        return 'repeat(4, 1fr)';
       case 5:
-        return 'repeat(5, 1fr)';       
+        return 'repeat(5, 1fr)';
       case 6:
-        return 'repeat(6, 1fr)';       
+        return 'repeat(6, 1fr)';
       default:
-        return 'repeat(2, 1fr)'
+        return 'repeat(1, 1fr)'
     }
   }
 }

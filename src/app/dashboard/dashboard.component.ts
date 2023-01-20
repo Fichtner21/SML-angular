09f75d4c0ecffb36da77a2b84ec4959e7aa69531
Fichtner21 <ernest.fichtner@gmail.com>
2023-01-20 17:14:03 +0100

    error 403 problem with scope in AuthConfig

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayersApiService, UserInfo } from '../services/players-api.service';
 
 
 @Component({
@@ -6,11 +7,37 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent { 
+  mailSnippets: string[] = []
+  userInfo?: UserInfo
 
-  constructor() { }
+  constructor(private readonly googleApi: PlayersApiService) { 
+    googleApi.userProfileSubject.subscribe( info => {
+      this.userInfo = info
+    })
+  }  
 
-  ngOnInit(): void {
+  isLoggedIn(): boolean {
+    return this.googleApi.isLoggedIn()
+  }
+
+  logout() {
+    this.googleApi.signOut()
+  }
+
+  async getEmails() {
+    if (!this.userInfo) {
+      return;
+    }
+
+    const userId = this.userInfo?.info.sub as string
+    const messages = await (this.googleApi.emails(userId)).toPromise()
+    messages.messages.forEach( (element: any) => {
+      const mail = (this.googleApi.getMail(userId, element.id)).toPromise()
+      mail.then( mail => {
+        this.mailSnippets.push(mail.snippet)
+      })
+    });
   }
 
 }
